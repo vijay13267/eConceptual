4e5a646ccc92be623283375256157e46f038e235
vijay13267 <vijaydonkena@gmail.com>
2024-08-04 20:34:16 +0530

    Jest Setup changes

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    './jest.setup.js'
+  ],
+  "transformIgnorePatterns": [
+    "node_modules/(?!(jest-)?react-native|@react-native|aws-amplify|@aws-amplify)"
+  ],
 };
